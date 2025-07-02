60e3d045cf2addd5121659bf17fc073b59efdce0
DiottoDev <nicodiottodev@gmail.com>
2025-07-02 18:58:12 -0300

    feat: add tab-context hook

---
+++ hooks/tab-context.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { create } from "zustand";
+
+/**
+ * Zustand store for global tab state management.
+ * Allows access and modification of the tab from anywhere in the app.
+ */
+interface TTabState {
+  tab: string;
+  setTab: (tab: string) => void;
+}
+
+/**
+ * Creates the Zustand store for the tab.
+ * The initial value is "emails" by default.
+ */
+export const useTabStore = create<TTabState>((set) => ({
+  tab: "emails",
+  setTab: (tab: string) => set({ tab }),
+}));
