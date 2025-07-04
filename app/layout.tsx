3c56b802630004fe7b66b61e6e4ab49a6f51c5d1
DiottoDev <nicodiottodev@gmail.com>
2025-07-03 23:20:53 -0300

    feat: integrate authentication provider in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "$/components/ui/sonner";
+import { AuthProvider } from "$/providers/auth-provider";
 import { ThemeProvider } from "$/providers/theme-provider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -6,11 +7,13 @@ import "./globals.css";
 const fontSans = Geist({
   variable: "--font-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const fontMono = Geist_Mono({
   variable: "--font-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({
@@ -33,7 +36,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <AuthProvider>{children}</AuthProvider>
           <Toaster />
         </ThemeProvider>
       </body>
