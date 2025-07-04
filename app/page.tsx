e6c6ce2de54e7aa4e096dfbefbd72a3a5b688726
DiottoDev <nicodiottodev@gmail.com>
2025-07-03 23:21:12 -0300

    feat: update home page with authentication integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import Dashboard from "$/components/dashboard";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Zenithly - Dashboard",
-};
-
 export default function Page() {
   return (
     <main className="flex h-full w-full items-center justify-center">
-      <Dashboard />
+      first page zenithly
     </main>
   );
 }
