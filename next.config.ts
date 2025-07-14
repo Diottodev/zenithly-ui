d2e142e1ba5785d51ed5c382d97eb2b6cadd333b
DiottoDev <nicodiottodev@gmail.com>
2025-07-14 01:48:23 -0300

    fix: update project configuration
    - Update package.json dependencies
    - Refresh pnpm lock file
    - Format Next.js configuration
    - Remove eslint config in favor of Biome

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,29 @@
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ['@workspace/ui'],
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
       },
     ],
   },
-};
+  rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: `${
+          process.env.BETTER_AUTH_URL || 'http://localhost:8080'
+        }/api/auth/:path*`,
+      },
+      {
+        source: '/api/users/:path*',
+        destination: `${
+          process.env.BETTER_AUTH_URL || 'http://localhost:8080'
+        }/api/users/:path*`,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
